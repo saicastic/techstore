45ffc27da90c50ddddc28adad6b53c2386e16404
saicastic <saiprithvisuperior@gmail.com>
2025-04-12 02:15:03 +0530

    add assets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div className="text-3xl">App</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
