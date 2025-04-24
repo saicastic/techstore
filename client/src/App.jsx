c502de6d61219cb0c84881da230d654b28a38b10
saicastic <saiprithvisuperior@gmail.com>
2025-04-25 01:24:02 +0530

    add footer Newsletter.jsx Login.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,18 @@ import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
+import { useAppContext } from "./context/AppContext";
+import Login from "./components/Login";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
+  const { showUserLogin } = useAppContext();
 
   return (
     <div>
       {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
       <Toaster />
       <div
         className={`${isSellerPath ? " " : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
@@ -18,6 +23,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
+      {!isSellerPath && <Footer />}
     </div>
   );
 };
