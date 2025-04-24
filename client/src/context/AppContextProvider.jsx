f9d9b4c5f2f3e17e711accb95d6e2a043731d1e2
saicastic <saiprithvisuperior@gmail.com>
2025-04-25 02:05:56 +0530

    add allproducts.jsx

--- client/src/context/AppContextProvider.jsx
+++ client/src/context/AppContextProvider.jsx
@@ -13,6 +13,7 @@ export const AppContextProvider = ({ children }) => {
   const [showUserLogin, setShowUserLogin] = useState(false);
   const [products, setProducts] = useState([]);
   const [cartItems, setCartItems] = useState({});
+  const [searchQuery, setSearchQuery] = useState({});
 
   // Fetch all products
   const fetchProducts = async () => {
@@ -72,6 +73,8 @@ export const AppContextProvider = ({ children }) => {
     addToCart,
     updateCartItems,
     removeFromCart,
+    searchQuery,
+    setSearchQuery,
   };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
