8c9da1678fee0052583fb49adfbdf6fbdfe2b689
saicastic <saiprithvisuperior@gmail.com>
2025-05-05 02:52:41 +0530

    add userRoute.js

---
+++ server/routes/userRoute.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  isAuth,
+  login,
+  logout,
+  register,
+} from "../controllers/userController.js";
+import authUser from "../middlewares/authUser.js";
+import "dotenv/config";
+
+const userRouter = express.Router();
+
+userRouter.post("/register", register);
+userRouter.post("/login", login);
+userRouter.get("/is-auth", authUser, isAuth);
+userRouter.get("/logout", authUser, logout);
+
+export default userRouter;
