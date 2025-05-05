4f4fe615272a1d4b2647e00ec9090dcc401a6796
saicastic <saiprithvisuperior@gmail.com>
2025-05-05 23:51:00 +0530

    add productController.js

---
+++ server/controllers/productController.js
@@ -0,0 +1,10 @@
+// Add Product : /api/proudct/add
+export const addProduct = async (req, res) => {};
+
+// Get Product : /api/proudct/list
+export const productList = async (req, res) => {};
+// Get Single Product : /api/proudct/id
+export const productById = async (req, res) => {};
+
+// change Product inStock : /api/proudct/stock
+export const changeStock = async (req, res) => {};
