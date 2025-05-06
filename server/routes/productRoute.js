2a62febb7242706641ae66fdbfea681f2695d259
saicastic <saiprithvisuperior@gmail.com>
2025-05-06 15:37:05 +0530

    add productRoute.js

---
+++ server/routes/productRoute.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { upload } from "../configs/multer";
+import authSeller from "../middlewares/authSeller";
+import {
+  addProduct,
+  changeStock,
+  productById,
+  productList,
+} from "../controllers/productController";
+
+const productRouter = express.Router();
+
+productRouter.post("/add", upload.array([images]), authSeller, addProduct);
+
+productRouter.get("/list", productList);
+productRouter.get("/id", productById);
+productRouter.post("/stock", authSeller, changeStock);
+
+export default productRouter;
