74ce5bd1a6892e9fbb8a91a85f626e52dcf4d6db
saicastic <saiprithvisuperior@gmail.com>
2025-05-06 15:42:09 +0530

    add orderRoute.js

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { placeOrderCOD } from "../controllers/OrderContoller";
+import { placeOrderCOD } from "../controllers/OrderContoller.js";
 
 const orderRouter = express.Router();
 
