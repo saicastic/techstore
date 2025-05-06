d971e9672f2b192dbf2b9ce7019d8bf2030dd6e0
saicastic <saiprithvisuperior@gmail.com>
2025-05-06 15:34:36 +0530

    modified server.js

--- server/server.js
+++ server/server.js
@@ -4,13 +4,19 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import "dotenv/config";
 import userRouter from "./routes/userRoute.js";
-import sellerRouter from "./routes/SellerRoute.js";
+import sellerRouter from "./routes/sellerRoute.js";
 import User from "./models/User.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import addressRouter from "./routes/adressRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 await connectDB();
+await connectCloudinary();
 
 // Allow multiple origins
 
@@ -45,6 +51,10 @@ app.get("/", (req, res) => {
 
 app.use("/api/user", userRouter);
 app.use("/api/seller", sellerRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/address", addressRouter);
+app.use("/api/order", orderRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
