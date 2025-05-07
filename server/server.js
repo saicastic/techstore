600d384c914ffd8c03cdc0e9acecf6f0542aba44
saicastic <saiprithvisuperior@gmail.com>
2025-05-07 19:22:32 +0530

    modified server.jsx

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import addressRouter from "./routes/addressRoute.js";
 import orderRouter from "./routes/orderRoute.js";
+import { stripeWebhooks } from "./controllers/orderContoller.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -21,6 +22,8 @@ await connectCloudinary();
 // Allow multiple origins
 
 const allowedOrigins = ["http://localhost:5173"];
+
+app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
 //Middle Ware Configuration
 
