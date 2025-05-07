dd85c8bf29b4468c82aee83b480d2d560150a570
saicastic <saiprithvisuperior@gmail.com>
2025-05-07 19:22:48 +0530

    modified addressController.jsx

--- server/controllers/addressController.js
+++ server/controllers/addressController.js
@@ -5,7 +5,8 @@ import Address from "../models/Address.js";
 export const addAddress = async (req, res) => {
   try {
     const { address, userId } = req.body;
-    await Address.create({ ...address }, userId);
+
+    await Address.create({ ...address, userId });
     res.json({ success: true, message: "Address added successfully" });
   } catch (error) {
     console.log(error.message);
