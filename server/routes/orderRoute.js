719ab1b05d3788cc57f279ac133d4fe289464668
saicastic <saiprithvisuperior@gmail.com>
2025-05-07 19:24:33 +0530

    modified OrderRoute.js

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,16 +1,19 @@
 import express from "express";
+
+import authUser from "../middlewares/authUser.js";
+import authSeller from "../middlewares/authSeller.js";
 import {
   getAllOrders,
   getUserOrders,
   placeOrderCOD,
-} from "../controllers/OrderContoller.js";
-import authUser from "../middlewares/authUser.js";
-import authSeller from "../middlewares/authSeller.js";
+  placeOrderStripe,
+} from "../controllers/orderContoller.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/cod", authUser, placeOrderCOD);
-orderRouter.post("/user", authUser, getUserOrders);
-orderRouter.post("/seller", authSeller, getAllOrders);
+orderRouter.post("/stripe", authUser, placeOrderStripe);
+orderRouter.get("/user", authUser, getUserOrders);
+orderRouter.get("/seller", authSeller, getAllOrders);
 
 export default orderRouter;
