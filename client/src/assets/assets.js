aeb5d457fd5fb9869b06bb73ca766206cc414825
saicastic <saiprithvisuperior@gmail.com>
2025-05-12 23:25:48 +0530

    changed link

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -20,7 +20,6 @@ import box_icon from "./box_icon.svg";
 import trust_icon from "./trust_icon.svg";
 import black_arrow_icon from "./black_arrow_icon.svg";
 import white_arrow_icon from "./white_arrow_icon.svg";
-import main_banner_bg_sm from "./main_banner_bg_sm.png";
 import bottom_banner_image from "./bottom_banner_image.png";
 import bottom_banner_image_sm from "./bottom_banner_image_sm.png";
 import add_address_iamge from "./add_address_image.svg";
